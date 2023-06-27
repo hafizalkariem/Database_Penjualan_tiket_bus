fdcb0d00eec3e731655a9747ed717fa6a76c28b6
yukikaru098 <dzakiabiyyu01@gmail.com>
2023-06-27 09:46:43 +0700

    dzaki commit

---
+++ penjualan_tiket_bus.sql
@@ -0,0 +1,13 @@
+create database penjualan_tiket_bus;
+create table penumpang 
+(id_penumpang varchar (10) primary key not null, nama varchar (45), no_tlp varchar (12) ) ;
+create table jadwal_berangkat
+(id_jadwal varchar(10) primary key not null, id_armada varchar(10), jam_berangkat time);
+create table transaksi
+(id_transaksi varchar(10) primary key not null, id_tiket varchar(10), tanggal date);
+create table armada
+(id_armada varchar(10) primary key not null,kelas varchar(10), id_rute varchar(10), jumlah_kursi numeric (12) );
+create table laporan_transaksi 
+(id_laporan varchar(10) primary key not null, tanggal date, jumlah_tiket numeric(12), total_pendapatan numeric(12) );
+create table Rute 
+( id_rute varchar(10), rute_awal varchar(10), rute_akhir varchar(10) );
